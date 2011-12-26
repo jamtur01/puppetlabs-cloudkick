e35cd753fadd31b364551d5dd59214ef5d04439f
James Turnbull <james@lovedthanlost.net>
2011-12-25 23:25:12 -0800

    Added support to add color to nodes
    Due to an issue with the API it appears
    new nodes don't get colors and tags. Issue
    logged with Cloudkick.

--- lib/puppet/type/cloudkick_node.rb
+++ lib/puppet/type/cloudkick_node.rb
@@ -20,8 +20,17 @@ Puppet::Type.newtype(:cloudkick_node) do
     desc "The IP address of the node."
   end
 
+  newproperty(:color) do
+    desc "The color to specify. Should be a Hex color code, ie. `#000000`."
+
+    validate do |value|
+      unless value =~ /^\#[A-Za-z0-9]+$/
+        raise ArgumentError , "%s is not a valid Hex color." % value
+      end
+    end
+  end
+
   newproperty(:tags, :array_matching => :all) do
     desc "Tags to be added to the Cloudkick node. Specify a tag or an array of tags."
-
   end
 end
